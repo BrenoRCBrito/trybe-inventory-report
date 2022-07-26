d99a8fc888b9d7ec5a8699029b4e1877afa1983a
BrenoRCBrito <brenorcbrito@gmail.com>
2022-07-26 20:48:04 -0300

    refactor: change test path :recycle:

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -12,16 +12,3 @@ def test_cria_produto():
     assert product.data_de_validade == "01-12-2020"
     assert product.numero_de_serie == "11111"
     assert product.instrucoes_de_armazenamento == "Geladeira"
-
-
-def test_relatorio_produto():
-    product = Product(
-        1, "Bolo", "Padaria", "01-01-2020", "01-12-2020", "11111", "Geladeira"
-    )
-    assert (
-        str(product.__repr__()) == f"O produto {product.nome_do_produto}"
-        f" fabricado em {product.data_de_fabricacao}"
-        f" por {product.nome_da_empresa} com validade"
-        f" até {product.data_de_validade}"
-        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
-    )
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,14 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, "Bolo", "Padaria", "01-01-2020", "01-12-2020", "11111", "Geladeira"
+    )
+    assert (
+        str(product.__repr__()) == f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
